86fd80ef1ad88b29cc8d5106a0338fcc133e968e
Chetan Mehta <mehtachetan707@gmail.com>
2020-08-23 21:11:30 +0530

    refactored the code

--- gcpclient/gcpclient.go
+++ gcpclient/gcpclient.go
@@ -1,4 +1,4 @@
-package gcpclient
+package main
 
 import (
 	"compress/gzip"
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"mime/multipart"
 	"net/http"
 	"strconv"
@@ -14,84 +13,68 @@ import (
 	"sync"
 	"time"
 
+	"github.com/gorilla/mux"
+
 	"cloud.google.com/go/storage"
-	"google.golang.org/api/option"
 )
 
 type storageConnection struct {
-	Client  *storage.Client
-	ConnErr error
+	Client *storage.Client
 }
 
 var (
 	client *storageConnection
+	once   sync.Once
 )
 
 const (
 	// GCSBucket name
 	GCSBucket = "GCSBucketName"
+	// ProjectID Google Project ID name
+	ProjectID = "YOUR_PROJECT_ID"
 	delimitor = "_"
 )
 
-var once sync.Once
+// GetGCSClient gets singleton object for Google Storage
+func GetGCSClient(ctx context.Context) (*storage.Client, error) {
+	var clientErr error
+	once.Do(func() {
+		storageClient, err := storage.NewClient(ctx)
+		if err != nil {
+			clientErr = fmt.Errorf("Failed to create GCS client ERROR:%s", err.Error())
+		} else {
+			client = &storageConnection{
+				Client: storageClient,
+			}
+		}
+	})
+	return client.Client, clientErr
+}
 
-// UploadFileData ...
-func UploadFileData(ctx context.Context, log *log.Logger, w http.ResponseWriter, r *http.Request, params map[string]string) error {
+// Upload API will take Multi Form data as an input and store the object to Google storag
+func Upload(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
 	err := r.ParseMultipartForm(100 << 20) // Max Size Limit is 100 MB
 	if err != nil {
-		return nil
+		fmt.Println("Error ", err.Error())
+		return
 	}
-	fhs := r.MultipartForm.File["FileUploadKey"]
-	var links []string
+	// fileKey is the name of key passed with mutli form request
+	fhs := r.MultipartForm.File["fileKey"]
+	// Multiple files can be passed as part of Multi form request
+	var fileLinks []string
 	for _, fh := range fhs {
-		link, err := UploadFileToGCSBucket(ctx, log, fh)
+		link, err := UploadFileToGCSBucket(ctx, fh)
 		if err != nil {
-			return nil
+			fmt.Println("Error ", err.Error())
+			return
 		}
-		links = append(links, link)
+		fileLinks = append(fileLinks, link)
 	}
-	// response := testrun.FileUploadResponse{Status: testrun.OK, Message: "Files Uploaded Successfully", Links: links}
-	// web.Respond(ctx, log, w, response, http.StatusOK)
-	return nil
 }
 
-// DownloadFileFromGCS gets a file from GCS bucket
-func DownloadFileFromGCS(ctx context.Context, log *log.Logger, w http.ResponseWriter, r *http.Request, params map[string]string) error {
-	clientCtx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	dir := params["dir"]
-	filename := params["filename"]
-	filePath := fmt.Sprintf("%s/%s", dir, filename)
-	client, err := GetGCSClient(clientCtx, log)
-	reader, err := client.Bucket(GCSBucket).Object(filePath).NewReader(clientCtx)
-	if err != nil {
-		return nil
-	}
-	defer reader.Close()
-	contentType := reader.ContentType()
-	size := strconv.FormatInt(reader.Size(), 10)
-	content, err := ioutil.ReadAll(reader)
-	if err != nil {
-		return nil
-	}
-	w.Header().Set("Content-Type", contentType)
-	disposition := "attachment"
-	if strings.Contains(contentType, "image") || strings.Contains(contentType, "pdf") {
-		disposition = "inline"
-	}
-	w.Header().Set("Content-Disposition", disposition+"; filename="+filename)
-	w.Header().Set("Content-Length", size)
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.WriteHeader(http.StatusOK)
-	writer := gzip.NewWriter(w)
-	defer writer.Close()
-	writer.Write(content)
-	return nil
-}
-
-// UploadFileToGCSBucket ...
-func UploadFileToGCSBucket(ctx context.Context, log *log.Logger, fh *multipart.FileHeader) (string, error) {
+// UploadFileToGCSBucket will create a date wise directory bucket
+func UploadFileToGCSBucket(ctx context.Context, fh *multipart.FileHeader) (string, error) {
 
 	date := time.Now()
 	dateStr := date.Format("01-02-2006") // MM-DD-YYYY Format
@@ -102,13 +85,13 @@ func UploadFileToGCSBucket(ctx context.Context, log *log.Logger, fh *multipart.F
 	}
 	defer file.Close()
 
-	filename := generateFileNameForGCS(ctx, log, fh.Filename)
+	filename := generateFileNameForGCS(ctx, fh.Filename)
 	filepath := fmt.Sprintf("%s/%s%s%s", dateStr, filename, delimitor, fh.Filename)
-	client, err := GetGCSClient(ctx, log)
+	client, err := GetGCSClient(ctx)
 	if err != nil {
 		return "", err
 	}
-	wc := client.Bucket(GCSBucket).Object(filepath).NewWriter(ctx)
+	wc := client.Bucket(GCSBucket).UserProject(ProjectID).Object(filepath).NewWriter(ctx)
 	if _, err = io.Copy(wc, file); err != nil {
 		return "", err
 	}
@@ -118,7 +101,9 @@ func UploadFileToGCSBucket(ctx context.Context, log *log.Logger, fh *multipart.F
 	return filepath, nil
 }
 
-func generateFileNameForGCS(ctx context.Context, log *log.Logger, name string) string {
+// generateFileNameForGCS will generate the resource path for a file.
+// It will use a combination of current time and filename to generate a unique entry.
+func generateFileNameForGCS(ctx context.Context, name string) string {
 	time := time.Now().UnixNano()
 	var strArr []string
 	strArr = append(strArr, name)
@@ -130,20 +115,48 @@ func generateFileNameForGCS(ctx context.Context, log *log.Logger, name string) s
 	return filename
 }
 
-// GetGCSClient singleton object ...
-func GetGCSClient(ctx context.Context, logger *log.Logger) (*storage.Client, error) {
-	var clientErr error
-	authKey := "" // JSON auth Key for accessing GCP resource
-	once.Do(func() {
-		storageClient, err := storage.NewClient(ctx, option.WithCredentialsJSON([]byte(authKey)))
-		if err != nil {
-			clientErr = fmt.Errorf("Failed to create GCS client ERROR:%s", err.Error())
-		} else {
-			client = &storageConnection{
-				Client:  storageClient,
-				ConnErr: err,
-			}
-		}
-	})
-	return client.Client, clientErr
+// Download gets a file from GCS bucket, Takes file path as a path param from request
+func Download(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Caling Download")
+	clientCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	dir := mux.Vars(r)["dir"]
+	filename := mux.Vars(r)["filename"]
+	filePath := fmt.Sprintf("%s/%s", dir, filename)
+	client, err := GetGCSClient(clientCtx)
+	reader, err := client.Bucket(GCSBucket).UserProject(ProjectID).Object(filePath).NewReader(clientCtx)
+	if err != nil {
+		fmt.Println("Error ", err.Error())
+		return
+	}
+	defer reader.Close()
+	contentType := reader.ContentType()
+	size := strconv.FormatInt(reader.Size(), 10)
+	content, err := ioutil.ReadAll(reader)
+	if err != nil {
+		fmt.Println("Error ", err.Error())
+		return
+	}
+	w.Header().Set("Content-Type", contentType)
+	disposition := "attachment"
+	if strings.Contains(contentType, "image") || strings.Contains(contentType, "pdf") {
+		disposition = "inline"
+	}
+	// The header field Content-Disposition helps the client(Browser)
+	// to decide whether the content can be rendered on the browser or downloaded.
+	w.Header().Set("Content-Disposition", disposition+"; filename="+filename)
+	w.Header().Set("Content-Length", size)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.WriteHeader(http.StatusOK)
+	writer := gzip.NewWriter(w)
+	defer writer.Close()
+	writer.Write(content)
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/upload", Upload).Methods("POST")
+	router.HandleFunc("/file/{dir}/{filename}", Download).Methods("GET")
+	http.ListenAndServe(":8081", router)
 }
